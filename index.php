60f490a97c9299ad4fcf1d3a5510f23b09870474
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-03-20 23:39:25 -0700

    rename partner

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ THE SOFTWARE.
         <div class="row no-gutters">
             <ul class="footer">
                 <li id="copyright"><a>© Victutors.com - VICTUTORS ALL RIGHTS RESERVED</a></li>
-                <li id="partner02" class="partner-pane"><a id="partner02.png" class="partner_tag" href="#">小戴续签</a></li>
+                <li id="partner02" class="partner-pane"><a id="partner02.JPG" class="partner_tag" href="#">维城续签服务</a></li>
                 <li id="partner01" class="partner-pane"><a id="partner01.JPG" class="partner_tag" href="#">维城生活信息馆</a></li>
                 <li class="partner-pane"><a href="#">合作伙伴 :</a></li>
             </ul>
