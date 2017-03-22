3608b5da26fc657fe3b0bc4d9a663414061a9445
Zihan Ye <zaeye1028@gmail.com>
2017-03-22 09:39:07 -0700

    1. nav bar color fixed 2. partner added

--- index.php
+++ index.php
@@ -105,6 +105,7 @@ THE SOFTWARE.
         <div class="row no-gutters">
             <ul class="footer">
                 <li id="copyright"><a>© Victutors.com - VICTUTORS ALL RIGHTS RESERVED</a></li>
+                <li id="partner03" class="partner-pane"><a id="partner03.JPG" class="partner_tag" href="#">维城便民生活</a></li>
                 <li id="partner02" class="partner-pane"><a id="partner02.JPG" class="partner_tag" href="#">维城续签服务</a></li>
                 <li id="partner01" class="partner-pane"><a id="partner01.JPG" class="partner_tag" href="#">维城生活信息馆</a></li>
                 <li class="partner-pane"><a href="#">合作伙伴 :</a></li>
