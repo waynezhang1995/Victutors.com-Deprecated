cd2e690e19bf71fa68df7cf133b29934298c877b
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-03-28 16:45:52 -0700

    footer and deploy script changes

--- Utils/deploy.sh
+++ Utils/deploy.sh
@@ -4,10 +4,10 @@
 
 copyDirectoryandFiles () {
     echo "\n\n******* Copying direcotry *******\n\n"
-    scp -r ./CSS/ victutors@victutors.com:public_html/
-    scp -r ./javascript/ victutors@victutors.com:public_html/
+    scp -r ../CSS/ victutors@victutors.com:public_html/
+    scp -r ../javascript/ victutors@victutors.com:public_html/
     echo "******* Copying php files *******\n\n"
-    scp ./*.* victutors@victutors.com:public_html/
+    scp ../*.* victutors@victutors.com:public_html/
 }
 
 echo "\n\n******* Deploy Script for Victutors by Wayne Zhang *******\n\n"
@@ -18,14 +18,14 @@ if [ -z "$1" ]; then
 elif [ "$1" = "js" ]; then
     echo " ====> Javascript option detected\n\n"
     echo "\n\n******* Copying Javascript files *******\n\n"
-    scp -r ./javascript/ victutors@victutors.com:public_html/
+    scp -r ../javascript/ victutors@victutors.com:public_html/
 elif [ "$1" = "css" ]; then
     echo " ====> CSS option detected\n\n"
     echo "\n\n******* Copying CSS files *******\n\n"
-    scp -r ./CSS/ victutors@victutors.com:public_html/
+    scp -r ../CSS/ victutors@victutors.com:public_html/
 elif [ "$1" = "php" ]; then
     echo "******* Copying php files *******\n\n"
-    scp ./*.* victutors@victutors.com:public_html/
+    scp ../*.* victutors@victutors.com:public_html/
 else
     echo " No such option ! Please try again\n  js ===> javascript\n css ===> CSS\n php ===> php files\n"
     exit 1
