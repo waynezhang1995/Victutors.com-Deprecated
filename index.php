8c521177f480202a9f10886c9af6244c2cabc8a4
Wayne Zhang <waynezhangyuwei@Gmail.com>
2017-04-03 18:35:23 -0700

    WIP handle alerts

--- index.php
+++ index.php
@@ -352,40 +352,6 @@
         <img src="./Images/Partner/partner01.jpg">
         </div>
     </div>
-
-    <div id="searchAlert" class="modal">
-        <div class="modal-content w3-animate-top w3-card-1">
-            <header class="w3-container w3-red">
-                <span onclick="document.getElementById('searchAlert').style.display='none'" class="w3-closebtn">&times;</span>
-                <h2 class="w3-red">错误！<br/>搜索科目不能为空，请重新输入</h2>
-            </header>
-        </div>
-    </div>
-
-    <div id="feedbackAlert" class="modal">
-        <div class="modal-content w3-animate-top w3-card-1">
-            <header class="w3-container w3-red">
-                <span onclick="document.getElementById('feedbackAlert').style.display='none'" class="w3-closebtn">&times;</span>
-                <h2 class="w3-red">谢谢您的反馈😊<br/>感谢您长期以来的支持！</h2>
-            </header>
-        </div>
-    </div>
-
-    <div id="newTutorAlert" class="modal">
-        <div class="modal-content w3-animate-top w3-card-1">
-            <header class="w3-container w3-red">
-                <span onclick="document.getElementById('newTutorAlert').style.display='none'" class="w3-closebtn">&times;</span>
-                <h2 class="w3-red">谢谢您的加入😊<br/>我们将在第一时间审核更新您的信息!<br/>您的支持是我们前进的动力！</h2>
-            </header>
-        </div>
-    </div>
-    <div id="newTutorMoreInfoAlert" class="modal">
-        <div class="modal-content w3-animate-top w3-card-1">
-            <header class="w3-container w3-red">
-                <span onclick="document.getElementById('newTutorMoreInfoAlert').style.display='none'" class="w3-closebtn">&times;</span>
-                <h2 class="w3-red">请填写完整信息😓<br/>更多的信息有助于学生联系您!<br/>您的支持是我们前进的动力！</h2>
-            </header>
-        </div>
-    </div>
+    <?php include 'Include/alerts.php'; ?>
 </body>
 </html>
